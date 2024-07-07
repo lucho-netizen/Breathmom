f10c9df9486d227ced7957e212e6949d3dafd3a3
lucho-netizen <codecors@hotmail.com>
2024-07-07 17:37:19 -0500

    Beta 10.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'nav', component: NavComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'logout', component: LogoutComponent},
+  { path: 'adduser', component: AdduserComponent },
+  { path: 'chatbot', component: ChatbotComponent },
+  { path: '**', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdduserComponent } from './components/adduser/adduser.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AdduserComponent,
+    LoginComponent,
+    AdminComponent,
+    ChatbotComponent,
+    FooterComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
+ 
+  bootstrap: [AppComponent],
+ 
   providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+       provideAnimationsAsync()
+ 
+  ]
 })
 export class AppModule { }
