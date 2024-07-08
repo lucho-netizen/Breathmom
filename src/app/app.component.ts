c79ca548cc5123bd10d3a89ae3141cbe5f95aa24
lucho-netizen <codecors@hotmail.com>
2024-07-08 13:38:49 -0500

    Beta 10.5.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'canAi';
+  imgUrls = '../assets/images/img.png'
 }
